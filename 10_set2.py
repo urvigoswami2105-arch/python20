bc564b1b4ed836bede59e7f6ed377fdd64b4af07
Urvi Goswami <urvigoswami2105@gmail.com>
2026-01-29 08:41:46 +0530

    Add files via upload

---
+++ 10_set2.py
@@ -0,0 +1,14 @@
+set1={1,2,3,4,5,6}
+set2={4,5,6,7,8,9}
+
+set3=set1.union(set2)
+print(set3)
+
+set4=set1.intersection(set2)
+print(set4)
+
+set5=set1.difference(set2)
+print(set5)
+
+set6=set2.difference(set1)
+print(set6)
---
+++ 2_lenght.py
@@ -0,0 +1,14 @@
+length = 0 
+width = 0 
+
+length=input("ENTER LENGTH :")
+width=input("ENTER WIDTH :")
+
+
+
+length=int(length)
+width=int(width)
+
+area=length*width
+
+print(area)
